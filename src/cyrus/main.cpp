521639a107177bd834d37a9ad99ff5940b09436b
BreeDurbin <sayamustfight@proton.me>
2024-03-20 06:37:18 -0500

    Added core qt event loop, setup exe to build to build folder

--- src/cyrus/main.cpp
+++ src/cyrus/main.cpp
@@ -1,8 +1,38 @@
 
 #include <iostream>
+#include <QCoreApplication>
+#include <string>
+#include <QTextStream>
+#include <QDebug>
+
+void do_cpp(){
+    std::string name;
+
+    std::cout << "cpp way - Please enter your name: " << std::endl;
+    std::getline(std::cin, name);
+    std::cout << "cpp out Hello: " << name << std::endl;
+}
+
+void do_qt(){
+    QTextStream qin(stdin);
+    QTextStream qout(stdout);
+
+    qout << "qt way - Please enter your name: "; 
+    qout.flush();
+    QString name = qin.readLine();
+    qout << "qout Hello: " << name << "\n";
+    qout.flush();
+}
 
 int main(int argc, char* argv[])
 {
-    std::cout << "hello world" << std::endl;
-    return 0;
+    QCoreApplication a(argc, argv);
+
+    std::cout << "Entering qt event loop" << std::endl;
+
+    do_cpp();
+
+    do_qt();
+
+    return a.exec();
 }
