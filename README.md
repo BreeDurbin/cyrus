b3d0848c480b5df7e833b4bd0888f4f54ed0ff5b
BreeDurbin <sayamustfight@proton.me>
2024-03-20 09:32:52 -0500

    Added main window.

--- README.md
+++ README.md
@@ -8,17 +8,21 @@
 ```
 
  
-A simple initiative tracker for d&amp;d and other ttrpg
+A simple initiative tracker for d&amp;d and other ttrpgs
 
 
-Dependencies(Learn these[youtube vids]):
+
+Dependencies:
+```
+g++
+make
 cmake 3.28.3
 qt6
 boost libraries 1.83.0
-
-
 ```
+
 To build:
+```
 # make build directory if doesn't already exist
 mkdir build
 cd build
--- src/cyrus/main.cpp
+++ src/cyrus/main.cpp
@@ -1,38 +1,19 @@
 
 #include <iostream>
-#include <QCoreApplication>
+#include <QApplication>
 #include <string>
 #include <QTextStream>
 #include <QDebug>
-
-void do_cpp(){
-    std::string name;
-
-    std::cout << "cpp way - Please enter your name: " << std::endl;
-    std::getline(std::cin, name);
-    std::cout << "cpp out Hello: " << name << std::endl;
-}
-
-void do_qt(){
-    QTextStream qin(stdin);
-    QTextStream qout(stdout);
-
-    qout << "qt way - Please enter your name: "; 
-    qout.flush();
-    QString name = qin.readLine();
-    qout << "qout Hello: " << name << "\n";
-    qout.flush();
-}
+#include "mainwindow.h"
 
 int main(int argc, char* argv[])
 {
-    QCoreApplication a(argc, argv);
+    QApplication a(argc, argv);
 
     std::cout << "Entering qt event loop" << std::endl;
 
-    do_cpp();
-
-    do_qt();
+    MainWindow w;
+    w.show();
 
     return a.exec();
 }
